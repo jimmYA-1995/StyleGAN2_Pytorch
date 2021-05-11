8d7eb161671f0b474863ad1e36d78ac0d94c5cbe
Chun-Ting, Ye <yct1995.cai08g@nctu.edu.tw>
2021-05-11 12:50:07 +0800

    tmp: mod reduce operation. not work on single GPU

--- run_training.py
+++ run_training.py
@@ -6,6 +6,7 @@ import argparse
 from time import time
 from pathlib import Path
 from pprint import pformat
+from collections import OrderedDict
 
 import wandb
 import torch
@@ -64,8 +65,9 @@ class Trainer():
     def __init__(self, args, cfg, logger):
         self.cfg = cfg
         self.log = logger
+        self.num_gpus = args.num_gpus
         self.local_rank = args.local_rank
-        self.distributed = args.num_gpus > 1
+        self.ddp = args.num_gpus > 1
         self.out_dir = args.out_dir
         self.use_wandb = args.wandb
         self.n_sample = cfg.N_SAMPLE
@@ -79,8 +81,8 @@ class Trainer():
         # Datset
         t = time()
         print("get dataloader ...", end='\r')
-        self.loader = get_dataloader(cfg, self.batch_size, distributed=self.distributed)
-        self.val_loader = get_dataloader(cfg, self.n_sample, split='val', distributed=self.distributed)
+        self.loader = get_dataloader(cfg, self.batch_size, distributed=self.ddp)
+        self.val_loader = get_dataloader(cfg, self.n_sample, split='val', distributed=self.ddp)
         print(f"get dataloader complete ({time() - t :.2f} sec)")
 
         # Define model
@@ -102,51 +104,31 @@ class Trainer():
         ).to(self.device)
 
         self.g_ema = copy.deepcopy(self.g).eval()
-        accumulate(self.g_ema, self.g, 0)
 
         # Define optimizers
-        g_reg_ratio = cfg_t.G_REG_EVERY / (cfg_t.G_REG_EVERY + 1)
-        d_reg_ratio = cfg_t.D_REG_EVERY / (cfg_t.D_REG_EVERY + 1)
-        self.g_optim = optim.Adam(
-            self.g.parameters(),
-            lr=cfg.TRAIN.LR * g_reg_ratio,
-            betas=(0 ** g_reg_ratio, 0.99 ** g_reg_ratio)
-        )
-        self.d_optim = optim.Adam(
-            self.d.parameters(),
-            lr=cfg.TRAIN.LR * d_reg_ratio,
-            betas=(0 ** d_reg_ratio, 0.99 ** d_reg_ratio)
-        )
+        g_reg_ratio = cfg.TRAIN.G_REG_EVERY / (cfg.TRAIN.G_REG_EVERY + 1)
+        d_reg_ratio = cfg.TRAIN.D_REG_EVERY / (cfg.TRAIN.D_REG_EVERY + 1)
+        self.g_optim = optim.Adam(self.g.parameters(), lr=cfg.TRAIN.LR * g_reg_ratio, betas=(0 ** g_reg_ratio, 0.99 ** g_reg_ratio))
+        self.d_optim = optim.Adam(self.d.parameters(), lr=cfg.TRAIN.LR * d_reg_ratio, betas=(0 ** d_reg_ratio, 0.99 ** d_reg_ratio))
 
         # resume from checkpoints if given
         self.start_iter = 0
         if cfg.TRAIN.NV_WEIGHTS_PATH:
-            load_weights_from_nv(
-                self.g, self.d, self.g_ema, cfg.TRAIN.NV_WEIGHTS_PATH)
-        elif cfg.TRAIN.CKPT:
+            raise RuntimeError("resume from NV model is deprecated because model architecture is different")
+
+        if cfg.TRAIN.CKPT:
+            # assume checkpoint will load on device directly.
             print(f'resume model from {cfg.TRAIN.CKPT}')
-            ckpt = torch.load(cfg.TRAIN.CKPT)
+            ckpt = torch.load(cfg.TRAIN.CKPT, map_location=self.device)
 
-            try:
-                self.start_iter = int(Path(cfg.TRAIN.CKPT).stem.split('-')[1])
-            except ValueError:
-                logger.info('**** load ckpt failed. start from scratch ****')
-                pass
+            self.g.load_state_dict(ckpt['g'])
+            self.d.load_state_dict(ckpt['d'])
+            self.g_ema.load_state_dict(ckpt['g_ema'])
 
-            try:
-                self.g.load_state_dict(ckpt['g'])
-                self.d.load_state_dict(ckpt['d'])
-                self.g_ema.load_state_dict(ckpt['g_ema'])
+            self.g_optim.load_state_dict(ckpt['g_optim'])
+            self.d_optim.load_state_dict(ckpt['d_optim'])
 
-                self.g_optim.load_state_dict(ckpt['g_optim'])
-                self.d_optim.load_state_dict(ckpt['d_optim'])
-            except RuntimeError:
-                logger.warn(
-                    " *** using hacky way to load partial weight to model *** ")
-                self.start_iter = load_partial_weights(
-                    self.g, self.d, self.g_ema, ckpt, logger=logger)
-
-        if self.distributed:
+        if self.ddp:
             self.g = nn.parallel.DistributedDataParallel(
                 self.g,
                 device_ids=[self.local_rank],
@@ -169,62 +151,42 @@ class Trainer():
     def train(self):
         cfg_d = self.cfg.DATASET
         cfg_t = self.cfg.TRAIN
-
-        digits_length = len(str(cfg_t.ITERATION))
-        loader = sample_data(self.loader)
-
         if self.start_iter >= cfg_t.ITERATION:
             print("the current iteration already meet your target iteration")
             return
 
+        digits_length = len(str(cfg_t.ITERATION))
+        ema_beta = 0.5 ** (self.batch_size * self.num_gpus / (10 * 1000))
+        mean_path_length = 0
+        loss_dict = OrderedDict(g=None, d=None, g_rec=None, real_score=None, fake_score=None,
+                                mean_path=None, r1=None, path=None, path_length=None)
+
+        # To make state_dict consistent in mutli nodes & single node training
+        g_module = self.g.module if self.ddp else self.g
+        d_module = self.d.module if self.ddp else self.d
+
+        sample_body_imgs, sample_face_imgs, sample_mask = [x.to(self.device) for x in next(iter(self.val_loader))]
+        sample_masked_body = sample_body_imgs * sample_mask
+        sample_z = torch.randn(self.n_sample, self.latent, device=self.device)
+        sample_label = torch.randint(self.num_classes, (sample_z.shape[0],)).to(self.device) if self.num_classes > 1 else None
+        self.log.debug(f"sample vector: {sample_z.shape}")
+
+        loader = sample_data(self.loader)
         pbar = range(self.start_iter, cfg_t.ITERATION)
         if get_rank() == 0:
-            pbar = tqdm(pbar, total=cfg_t.ITERATION,
-                        initial=self.start_iter, dynamic_ncols=True, smoothing=0.01)
-
-        # init. all
-        mean_path_length = 0
-        g_loss_val = 0
-        g_rec_loss_val = 0
-        d_loss_val = 0
-        r1_loss = torch.tensor(0.0, device=self.device)
-        path_loss = torch.tensor(0.0, device=self.device)
-        path_lengths = torch.tensor(0.0, device=self.device)
-        mean_path_length_avg = 0
-        loss_dict = {}
-
-        if self.distributed:
-            g_module = self.g.module
-            d_module = self.d.module
-        else:
-            g_module = self.g
-            d_module = self.d
-
-        accum = 0.5 ** (32 / (10 * 1000))
-
-        val_loader = sample_data(self.val_loader)
-        sample_body_imgs, sample_face_imgs, sample_mask = [
-            x.to(self.device) for x in next(val_loader)]
-        sample_masked_body = sample_body_imgs * sample_mask
-        del val_loader
-        sample_z = torch.randn(self.n_sample, self.latent, device=self.device)
-        sample_label = (torch.randint(self.num_classes, (sample_z.shape[0],)).to(self.device)
-                        if self.num_classes > 1 else None)
-        self.log.debug(f"sample vector: {sample_z.shape}")
+            pbar = tqdm(pbar, total=cfg_t.ITERATION, initial=self.start_iter, dynamic_ncols=True, smoothing=0.01)
 
         # main loop
         for i in pbar:
             s = time()
-            body_imgs, face_imgs, mask = [
-                x.to(self.device) for x in next(loader)]
+            body_imgs, face_imgs, mask = [x.to(self.device) for x in next(loader)]
             masked_body = body_imgs * mask
 
             requires_grad(self.g, False)
             requires_grad(self.d, True)
 
             noise = mixing_noise(self.batch_size, self.latent, cfg_t.STYLE_MIXING_PROB, self.device)
-            fake_label = (torch.randint(self.num_classes, (self.batch_size,)).to(self.device)
-                          if self.num_classes > 1 else None)
+            fake_label = torch.randint(self.num_classes, (self.batch_size,)).to(self.device) if self.num_classes > 1 else None
             fake_img, _ = self.g(noise, labels_in=fake_label, style_in=face_imgs, content_in=masked_body)
             fake_pred = self.d(fake_img)
             real_pred = self.d(body_imgs)
@@ -239,8 +201,7 @@ class Trainer():
             d_loss.backward()
             self.d_optim.step()
 
-            d_regularize = i % cfg_t.D_REG_EVERY == 0
-            if d_regularize:
+            if i % cfg_t.D_REG_EVERY == 0:
                 body_imgs.requires_grad = True
                 real_pred = self.d(body_imgs)
                 r1_loss = d_r1_loss(real_pred, body_imgs)
@@ -248,22 +209,17 @@ class Trainer():
                 self.d.zero_grad()
                 (cfg_t.R1 / 2 * r1_loss * cfg_t.D_REG_EVERY + 0 * real_pred[0]).backward()
                 self.d_optim.step()
-
-            loss_dict['r1'] = r1_loss
+                loss_dict['r1'] = r1_loss
 
             requires_grad(self.g, True)
             requires_grad(self.d, False)
 
-            noise = mixing_noise(
-                self.batch_size, self.latent, cfg_t.STYLE_MIXING_PROB, self.device)
-            fake_label = (torch.randint(self.num_classes, (self.batch_size,)).to(self.device)
-                          if self.num_classes > 1 else None)
-            fake_img, _ = self.g(
-                noise, labels_in=fake_label, style_in=face_imgs, content_in=masked_body)
+            noise = mixing_noise(self.batch_size, self.latent, cfg_t.STYLE_MIXING_PROB, self.device)
+            fake_label = torch.randint(self.num_classes, (self.batch_size,)).to(self.device) if self.num_classes > 1 else None
+            fake_img, _ = self.g(noise, labels_in=fake_label, style_in=face_imgs, content_in=masked_body)
             fake_pred = self.d(fake_img)
             g_loss = nonsaturating_loss(fake_pred)
-            g_rec_loss = masked_l1_loss(
-                masked_body, fake_img, mask=mask) * 256 * 256
+            g_rec_loss = masked_l1_loss(masked_body, fake_img, mask=mask) * self.cfg.RESOLUTION ** 2
             loss_dict['g'] = g_loss
             loss_dict['g_rec'] = g_rec_loss
 
@@ -271,79 +227,59 @@ class Trainer():
             (g_loss + g_rec_loss).backward()
             self.g_optim.step()
 
-            g_regularize = i % cfg_t.G_REG_EVERY == 0
-
-            if g_regularize:
+            if i % cfg_t.G_REG_EVERY == 0:
                 self.log.debug("Apply regularization to G")
-                path_batch_size = max(
-                    1, self.batch_size // cfg_t.PATH_BATCH_SHRINK)
-                noise = mixing_noise(
-                    path_batch_size, self.latent, cfg_t.STYLE_MIXING_PROB, self.device
-                )
+                self.g.zero_grad()
+                path_batch_size = max(1, self.batch_size // cfg_t.PATH_BATCH_SHRINK)
+                noise = mixing_noise(path_batch_size, self.latent, cfg_t.STYLE_MIXING_PROB, self.device)
                 fake_label = (torch.randint(self.num_classes, (path_batch_size,)).to(self.device)
                               if self.num_classes > 0 else None)
                 fake_img, latents = self.g(
                     noise, labels_in=fake_label, style_in=face_imgs[:path_batch_size], content_in=masked_body[:path_batch_size], return_latents=True)
 
-                path_loss, mean_path_length, path_lengths = path_regularize(
-                    fake_img, latents, mean_path_length
-                )
-
-                self.g.zero_grad()
+                path_loss, mean_path_length, path_lengths = path_regularize(fake_img, latents, mean_path_length)
                 weighted_path_loss = cfg_t.PATH_REGULARIZE * cfg_t.G_REG_EVERY * path_loss
 
                 if cfg_t.PATH_BATCH_SHRINK:
                     weighted_path_loss += 0 * fake_img[0, 0, 0, 0]
 
                 weighted_path_loss.backward()
-
                 self.g_optim.step()
 
-                mean_path_length_avg = (
-                    reduce_sum(mean_path_length).item() / get_world_size()
-                )
+                loss_dict['path'] = path_loss
+                loss_dict['path_length'] = path_lengths.mean()
+                loss_dict['mean_path'] = mean_path_length
 
-            loss_dict['path'] = path_loss
-            loss_dict['path_length'] = path_lengths.mean()
-
-            accumulate(self.g_ema, g_module, accum)
+            accumulate(self.g_ema, g_module, ema_beta)
 
             if (get_rank() == 0 and i != 0 and (i + 1) % self.cfg.EVAL.FID.EVERY == 0):
                 k_iter = (i + 1) / 1000
                 self.g_ema.eval()
                 self.fid_tracker.calc_fid(self.g_ema, k_iter, save=True)
 
-            loss_reduced = reduce_loss_dict(loss_dict)
+            # reduce loss
+            with torch.no_grad():
+                losses_list = torch.stack(list(loss_dict.values()), dim=0)
+                torch.distributed.reduce_multigpu(losses_list, dst=0)
 
-            d_loss_val = loss_reduced['d'].mean().item()
-            g_loss_val = loss_reduced['g'].mean().item()
-            g_rec_loss_val = loss_reduced['g_rec'].mean().item()
-            r1_val = loss_reduced['r1'].mean().item()
-            path_loss_val = loss_reduced['path'].mean().item()
-            real_score_val = loss_reduced['real_score'].mean().item()
-            fake_score_val = loss_reduced['fake_score'].mean().item()
-            path_length_val = loss_reduced['path_length'].mean().item()
+            if self.local_rank == 0:
+                reduced_loss = {k: (v / self.num_gpus).item()
+                                for k, v in zip(loss_dict.keys(), losses_list[0])}
 
-            if get_rank() == 0:
                 pbar.set_description(
-                    (
-                        f'd: {d_loss_val:.4f}; g: {g_loss_val:.4f}; g_rec: {g_rec_loss_val:.4f}; r1: {r1_val:.4f}; '
-                        f'path: {path_loss_val:.4f}; mean path: {mean_path_length_avg:.4f}'
-                    )
+                    """d: {d:.4f}; g: {g:.4f}; g_rec: {g_rec:.4f}; r1: {r1:.4f}; path: {path:.4f}; mean path: {mean_path:.4f}
+                    """.format(**reduced_loss)
                 )
 
                 if i == 0 or (i + 1) % cfg_t.SAMPLE_EVERY == 0:
-                    sample_iter = 'init' if i == 0 else str(
-                        i).zfill(digits_length)
+                    sample_iter = 'init' if i == 0 else str(i).zfill(digits_length)
                     with torch.no_grad():
                         self.g_ema.eval()
-                        sample, _ = self.g_ema(
-                            [sample_z], labels_in=sample_label, style_in=sample_face_imgs, content_in=sample_masked_body)
+                        sample, _ = self.g_ema([sample_z], labels_in=sample_label, style_in=sample_face_imgs, content_in=sample_masked_body)
 
                         if cfg_d.DATASET == 'MultiChannelDataset':
                             sample_list = torch.split(sample, cfg_d.CHANNELS, dim=1)
-                            sample_list = [(x.repeat(1, 3, 1, 1) if x.shape[1] == 1 else x)
-                                           for x in sample_list]
+                            sample_list = [(x.repeat(1, 3, 1, 1) if x.shape[1] == 1 else x)for x in sample_list]
 
                             utils.save_image(
                                 list(torch.cat(sample_list, dim=2).unbind(0)),
@@ -355,10 +291,8 @@ class Trainer():
 
                         else:
                             b, c, h, w = sample.shape
-                            stack_samples = torch.stack(
-                                [sample_face_imgs, sample_body_imgs, sample], dim=0)
-                            samples = torch.transpose(
-                                stack_samples, 0, 1).reshape(3 * b, c, h, w)
+                            stack_samples = torch.stack([sample_face_imgs, sample_body_imgs, sample], dim=0)
+                            samples = torch.transpose(stack_samples, 0, 1).reshape(3 * b, c, h, w)
                             utils.save_image(
                                 samples,
                                 self.out_dir / f'samples/fake-{sample_iter}.png',
@@ -388,15 +322,15 @@ class Trainer():
                 if wandb and self.use_wandb:
                     wandb.log({
                         'training time': time() - s,
-                        'Generator': g_loss_val,
-                        'G-reconstruction': g_rec_loss_val,
-                        'Discriminator': d_loss_val,
-                        'R1': r1_val,
-                        'Path Length Regularization': path_loss_val,
-                        'Mean Path Length': mean_path_length,
-                        'Real Score': real_score_val,
-                        'Fake Score': fake_score_val,
-                        'Path Length': path_length_val,
+                        'Generator': reduced_loss['g'],
+                        'G-reconstruction': reduced_loss['g_rec'],
+                        'Discriminator': reduced_loss['d'],
+                        'R1': reduced_loss['r1'],
+                        'Path Length Regularization': reduced_loss['path'],
+                        'Path Length': reduced_loss['path_length'],
+                        'Mean Path Length': reduced_loss['mean_path'],
+                        'Real Score': reduced_loss['real_score'],
+                        'Fake Score': reduced_loss['fake_score'],
                     })
 
         if args.local_rank == 0 and self.fid_tracker:
@@ -425,7 +359,7 @@ if __name__ == '__main__':
 
         print = master_only(print)
         print("print function overriden")
-    
+
     args.out_dir = None
     if args.num_gpus == 1 or args.local_rank == 0:
         misc.prepare_training(args, cfg)
