323a4ff2e3a57b437bebb0b4604856716af45676
JimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2021-05-16 20:27:16 +0800

    compare 2 version of FID detector

--- metrics/fid.py
+++ metrics/fid.py
@@ -3,6 +3,7 @@ import sys
 import time
 import copy
 import json
+import random
 import pickle
 import logging
 from pathlib import Path
@@ -27,6 +28,12 @@ def sample_data(loader):
         for batch in loader:
             yield batch
 
+            
+def seed_worker(worker_id):
+    torch.manual_seed(1234)
+    np.random.seed(1234)
+    random.seed(1234)
+            
 
 def load_condition_sample(sample_dir, batch_size):
     IMG_EXTS = ['jpg', 'png', 'jpeg']
@@ -88,6 +95,8 @@ class FIDTracker():
             if self.rank != 0:
                 torch.distributed.barrier()
             self.inceptionV3 = load_patched_inception_v3().eval().to(self.device)
+            # with open('/home/u3534611/.cache/dnnlib/downloads/a866f8d678872dcf6fcf60ddd09807ab_https___nvlabs-fi-cdn.nvidia.com_stylegan2-ada-pytorch_pretrained_metrics_inception-2015-12-05.pt', 'rb') as f:
+                # self.inceptionV3 = torch.jit.load(f).eval().to(self.device)
             if self.rank == 0:
                 torch.distributed.barrier()
         self.log.info("load inception model complete ({:.2f})".format(time.time() - start))
@@ -144,7 +153,7 @@ class FIDTracker():
         finish = time.time()
         total_time = finish - start
         self.log.info(f'FID in {str(1000 * k_iter).zfill(6)} \
-             iterations: "{fids}". [costs {round(total_time - start, 2)} sec(s)]')
+             iterations: "{fids}". [costs {round(total_time, 2)} sec(s)]')
         self.k_iters.append(k_iter)
         self.fids.append(fids)
 
@@ -178,7 +187,7 @@ class FIDTracker():
             self.log.info(f"#item subset: {len(item_subset)}")
 
             dataloader = torch.utils.data.DataLoader(
-                dataset=dataset, sampler=item_subset, batch_size=4, num_workers=2)
+                dataset=dataset, sampler=item_subset, batch_size=4, num_workers=2, worker_init_fn=seed_worker)
 
             features = []
             loader = iter(dataloader)
@@ -194,6 +203,7 @@ class FIDTracker():
                 self.log.debug(f"imgs: {imgs.shape}")
                 imgs = imgs.to(self.device)
                 feature = self.inceptionV3(imgs)[0].view(imgs.shape[0], -1)
+                # feature = self.inceptionV3(torch.clamp(imgs * 127.5 + 127.5, 0, 255).to(torch.uint8).to(self.device), return_features=True)
                 self.log.debug(f"feature: {feature.shape}")
                 if self.num_gpus > 1:
                     _features = []
@@ -223,7 +233,8 @@ class FIDTracker():
             loader = torch.utils.data.DataLoader(self.val_dataset,
                                                  batch_size=self.model_bs,
                                                  shuffle=True,
-                                                 num_workers=2)
+                                                 num_workers=2,
+                                                 worker_init_fn=seed_worker)
             loader = sample_data(loader)
             features = []
             idx_iterator = range(n_batch + 1)
@@ -247,6 +258,7 @@ class FIDTracker():
 
                 imgs, _ = generator([latent], labels_in=fake_label, style_in=face_imgs, content_in=masked_body)
                 feature = self.inceptionV3(imgs[:, :3, :, :])[0].view(imgs.shape[0], -1)
+                # feature = self.inceptionV3(torch.clamp(imgs * 127.5 + 127.5, 0, 255).to(torch.uint8).to(self.device), return_features=True)
                 if self.num_gpus > 1:
                     _features = []
                     for src in range(self.num_gpus):
@@ -273,6 +285,8 @@ class FIDTracker():
 
 
 def subprocess_fn(rank, args, cfg, temp_dir):
+    torch.manual_seed(1234)
+    np.random.seed(1234)
     from models import Generator
     args.local_rank = rank
     if args.num_gpus > 1:
