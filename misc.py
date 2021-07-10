e2f658966b44c556f22c6084b1c597ae6c372c93
jimmYA-1995 <yct1995.cai08g@nctu.edu.tw>
2021-07-10 19:27:25 +0800

    do nothing when outdir is None

--- misc.py
+++ misc.py
@@ -50,9 +50,11 @@ class CustomFormatter(logging.Formatter):
 
 
 def create_logger(local_rank, out_dir=None, debug=False, **kwargs):
-    if out_dir is not None and not isinstance(out_dir, Path):
-        out_dir = Path(out_dir)
-    out_dir.mkdir(exist_ok=True, parents=True)
+    if out_dir is not None:
+        if not isinstance(out_dir, Path):
+            out_dir = Path(out_dir)
+        out_dir.mkdir(exist_ok=True, parents=True)
+
     logger_name = f"GPU{local_rank}"
     loglevel = 'DEBUG' if debug else ('INFO' if local_rank == 0 else 'WARN')
 
