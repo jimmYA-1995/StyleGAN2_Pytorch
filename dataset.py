018bd918e6d135977732a5f271696c2ef2458676
Chun-Ting, Ye <yct1995.cai08g@nctu.edu.tw>
2021-07-11 19:11:34 +0800

    delete multichannel dataset

--- dataset.py
+++ dataset.py
@@ -2,14 +2,12 @@ import pickle
 from time import time
 from io import BytesIO
 from pathlib import Path
-from functools import partial
 from collections import namedtuple
 
 import lmdb
 import cv2
 import torch
 import numpy as np
-from tqdm import tqdm
 from PIL import Image
 from torch.utils import data
 from torchvision import transforms
@@ -69,115 +67,6 @@ def ImageFolderDataset(config, resolution, transform=None):
         return False
 
     return ImageFolder(config.roots[0], transform=transform, loader=image_loader, is_valid_file=check_valid)
-
-
-def load_images_and_concat(path, resolution, sources, channel_info=None, flip=False):
-    # assert isinstance(paths, (tuple, list))
-    src_path = str(path)
-    try:
-        imgs = []
-        for i, src in enumerate(sources):
-            path = src_path.replace(sources[0], src)
-            if not Path(path).exists():
-                raise RuntimeError(f'Path {path} does not exists. Please check all your sources \
-                                   in dataset are match')
-            img = Image.open(path).resize((resolution, resolution), Image.ANTIALIAS)
-            if flip:
-                img = img.transpose(Image.FLIP_LEFT_RIGHT)
-
-            img = np.array(img)
-            if img.ndim == 2:
-                img = img[..., None]
-            if channel_info:
-                assert img.shape[-1] == channel_info[i]
-            imgs.append(img)
-    except:
-        raise RuntimeError(f'fail to load the image: {path}')
-
-    cat_images = np.concatenate(imgs, axis=-1)
-
-    return cat_images
-
-
-class MultiChannelDataset(data.Dataset):
-    """
-    This dataset concatenate the source images with other
-    information images like skeletons or masks.
-    """
-    def __init__(self, config, resolution, transform=None, **kwargs):
-        from torchvision import get_image_backend
-        assert get_image_backend() == 'PIL'
-        assert len(config.source) == len(config.channels), \
-            f"the numbers of sources and channels don't match."
-
-        self.roots = config.roots
-        self.transform = transform
-        self.target_transform = None
-        self.loader = partial(load_images_and_concat,
-                              resolution=resolution,
-                              sources=config.source,
-                              channel_info=config.channels)
-        self.load_in_mem = config.LOAD_IN_MEM
-        self.flip = config.xflip
-
-        sources = config.source
-        self.img_paths = []
-        for root in self.roots:
-            root = Path(root)
-            for src in sources:
-                assert (root / src).is_dir(), f'source directory {src} is not in root path: {root}'
-
-            self.img_paths.extend(list((root / sources[0]).glob('*.jpg')))
-
-        self.length = len(self.img_paths)
-        if self.flip:
-            self.length *= 2
-
-        if self.load_in_mem:
-            print('Loading all images into memory...')
-            self.data, self.labels = [], []
-            for index in tqdm(range(self.length)):
-                path = self.img_paths[index % (self.length // 2)]
-                target = None  # unconditional for now
-                flip = index >= (self.length // 2)
-                concat_img = self.loader(path, flip=flip)
-                if self.transform is not None:
-                    concat_img = self.transform(concat_img)
-                # if self.target_transform is not None:
-                #     target = self.target_transform(target)
-                self.data.append(concat_img)
-                self.labels.append(target)
-
-    def __len__(self):
-        return self.length
-
-    def __getitem__(self, index):
-        if self.load_in_mem:
-            concat_img = self.data[index]
-            target = self.labels[index]
-        else:
-            path = self.img_paths[index % (self.length // 2)]
-            flip = index >= (self.length // 2)
-            concat_img = self.loader(path, flip=flip)
-            target = 0
-
-            if self.transform is not None:
-                concat_img = self.transform(concat_img)
-
-            if self.target_transform is not None:
-                target = self.target_transform(target)
-
-        return concat_img, target
-
-    def __repr__(self):
-        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
-        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
-        fmt_str += '    Root Location: {}\n'.format(self.roots)
-        tmp = '    Transforms (if any): '
-        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        tmp = '    Target Transforms (if any): '
-        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
-        return fmt_str
 
 
 class GenericDataset(data.Dataset):
@@ -395,12 +284,14 @@ def get_dataloader(config, batch_size, n_workers=None, split='train', distribute
 
 
 def get_dataloader_for_each_class(config, batch_size, distributed=False):
+    """ map each class into a corresponding dataloader
+    """
     dataset = get_dataset(config.DATASET, config.resolution)
     data_root = Path(config.DATASET.roots[0])
     dataloaders = []
     indices = list(range(len(dataset)))
     last_idx, cur_idx = 0, 0
-    for i, (label_class, idx) in enumerate(dataset.class_to_idx.items(), 1):
+    for label_class, idx in dataset.class_to_idx.items():
         for ext in ALLOW_EXTS:
             cur_idx += len(list((data_root / label_class).glob(f'*.{ext}')))
 
